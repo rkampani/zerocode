6dc4551107aa5e988504f0a1fe3ef15c052dff3f
rkampani <rkampani@gmail.com>
2025-04-30 12:49:40 -0500

    fix

--- core/src/test/java/org/jsmart/zerocode/core/envprop/suite/NewPortTestZeroCodeUnitRunner.java
+++ core/src/test/java/org/jsmart/zerocode/core/envprop/suite/NewPortTestZeroCodeUnitRunner.java
@@ -4,13 +4,17 @@ import org.jsmart.simulator.main.SimpleRestJsonSimulatorsMain;
 import org.jsmart.zerocode.core.runner.ZeroCodePackageRunner;
 import org.jsmart.zerocode.core.utils.SmartUtils;
 import org.junit.runners.model.InitializationError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NewPortTestZeroCodeUnitRunner extends ZeroCodePackageRunner {
     SimpleRestJsonSimulatorsMain simulator ;
     public static final int PORT = 9997;
+    private static final Logger logger = LoggerFactory.getLogger(NewPortTestZeroCodeUnitRunner.class);
 
     public NewPortTestZeroCodeUnitRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
+        logger.info("#################starting SimpleRestJsonSimulatorsMain with port "+PORT);
         simulator = new SimpleRestJsonSimulatorsMain(PORT);
         simulator.start();
     }
